b0414485c712b97cc895cc850afaa40b28a95e3e
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2017-12-13 22:59:32 -0800

    removed identifier in for loop

---
+++ aboutMe.js
@@ -0,0 +1,5 @@
+var aboutMe = {
+	
+	"part1":"I'm Bahar. I'm passionate about developing technical solutions that thrill users. Designing elegant software that tackles challenging problems is what I do. I love Computer Science because it makes me want to pull my hair out, jump for joy, and cry tears of sleep deprivation....all at once.", 
+	"part2":"As a third year at Northeastern University, I have completed my first co-op at Intuit in Mountain View California where I worked on Quickbooks Online's Payroll API. Fun fact: I won the creative writing award in high school and love writing poetry. Check out some of my projects below!"
+};
